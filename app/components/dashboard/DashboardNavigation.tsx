1df917f51005035e76c1c6d0646c010e3ad5c4c9
khoms <sundp.kshetri@gmail.com>
2024-07-02 00:25:09 +0545

    Create a responsive navbar

---
+++ app/components/dashboard/DashboardNavigation.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+const links = [
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+  },
+  {
+    name: "Orders",
+    href: "/dashboard/orders",
+  },
+  {
+    name: "Products",
+    href: "/dashboard/products",
+  },
+  {
+    name: "Categories",
+    href: "/dashboard/categories",
+  },
+];
+
+export function DashboardNavigation() {
+  return (
+    <>
+      {links.map((link) => {
+        return (
+          <Link key={link.href} href={link.href}>
+            {link.name}
+          </Link>
+        );
+      })}
+    </>
+  );
+}
