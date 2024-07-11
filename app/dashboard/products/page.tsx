7ff8f82253fddb2138123f92a4821ad39b52d1b4
khoms <sundp.kshetri@gmail.com>
2024-07-12 00:03:09 +0545

    Create product page design

---
+++ app/dashboard/products/page.tsx
@@ -0,0 +1,88 @@
+import { Button } from "@/components/ui/button";
+import { MoreHorizontal, PlusCircle, UserIcon } from "lucide-react";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+
+export default function ProductsRoute() {
+  return (
+    <>
+      <div className="flex items-center justify-end">
+        <Button asChild className="flext items-center gap-x-2">
+          <Link href="/dashboard/products/create">
+            <PlusCircle className="size-4.5" />
+            <span>Add Product</span>
+          </Link>
+        </Button>
+      </div>
+
+      <Card className="mt-5">
+        <CardHeader className="px-7">
+          <CardTitle>Products</CardTitle>
+          <CardDescription>
+            Manage your products and view their sales and performance
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="">Image</TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Price</TableHead>
+                <TableHead>Date</TableHead>
+                <TableHead className="text-end">Actions</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <UserIcon className="size-16" />
+                </TableCell>
+                <TableCell>Nike Air Jordan</TableCell>
+                <TableCell>$299.00</TableCell>
+                <TableCell>2024-04-08</TableCell>
+                <TableCell className="text-right flex gap-2">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button size="icon" variant="ghost">
+                        <MoreHorizontal className="size-4" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>Edit</DropdownMenuItem>
+                      <DropdownMenuItem>Delete</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
